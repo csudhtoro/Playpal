a0abb4cd05aaebd7b5c299b86d7b93ad6f90d1f5
Dana Shorts <csudhtoro@hotmail.com>
2024-02-28 18:42:40 -0800

    updated submenu hover

--- src/components/Navbar/DropdownMenu.jsx
+++ src/components/Navbar/DropdownMenu.jsx
@@ -8,13 +8,13 @@ const DropdownMenu = ({ signOut }) => {
     <div className="bg-gray-50 flex flex-col absolute top-[1rem] sm:top-[0.8rem] right-[0.5rem] md:w-[120px] p-4 rounded-md border border-gray-300 shadow-sm shadow-gray-300">
       <ul className="flex flex-col gap-2 items-center justify-center">
         <li
-          className="px-2 w-16 sm:w-18 md:w-20 text-xs font-medium md:text-sm text-[#0356fc] cursor-pointer hover:bg-gray-200 flex justify-between items-center"
+          className="px-2 w-16 sm:w-18 md:w-20 text-xs font-medium md:text-sm text-[#0356fc] cursor-pointer hover:bg-gray-200 flex justify-between items-center rounded-md"
           onClick={() => router.push("/profile")}
         >
           Profile
         </li>
         <li
-          className="px-2 w-16 sm:w-18 md:w-20 text-xs font-medium md:text-sm text-[#0356fc] cursor-pointer hover:bg-gray-200 flex justify-between items-center"
+          className="px-2 w-16 sm:w-18 md:w-20 text-xs font-medium md:text-sm text-[#0356fc] cursor-pointer hover:bg-gray-200 flex justify-between items-center rounded-md"
           onClick={signOut}
         >
           Logout
