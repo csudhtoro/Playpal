57733bdcec5f300c682b1424f422297e293ab0f2
Dana Shorts <csudhtoro@hotmail.com>
2024-03-01 14:27:58 -0800

    updated tab title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,15 +4,23 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import { AuthContextProvider } from "@/context/AuthUserContext";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps: { ...pageProps } }) {
   return (
-    <div>
-      <AuthContextProvider>
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </AuthContextProvider>
-    </div>
+    <>
+      <Head>
+        <title>Playpal</title>
+        <meta name="description" content="A place to meet activity partners" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <AuthContextProvider>
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </AuthContextProvider>
+      </div>
+    </>
   );
 }
