be6ac5f3d64e7e89072efc6e42c6bf73921a4003
Dana Shorts <csudhtoro@hotmail.com>
2024-03-01 10:52:22 -0800

    updated metadata for tab title

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,11 @@ import Navbar from "@/components/Navbar/Navbar";
 import { AuthContextProvider } from "@/context/AuthUserContext";
 import "@/styles/globals.css";
 
+export const metadata = {
+  title: "PlayPal | Make Connections",
+  description: "A place to connect with others with active lifestyles"
+};
+
 export default function App({ Component, pageProps: { ...pageProps } }) {
   return (
     <div>
